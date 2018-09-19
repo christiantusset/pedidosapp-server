4bd3e0c61d3d033a289044e70b1b8358ac7180ea
Christian Tusset <christiantusset@gmail.com>
2018-09-19 00:46:28 -0300

    Operação para Instanciar Categorias

--- src/main/java/com/christiantusset/pedidosapp/PedidosappApplication.java
+++ src/main/java/com/christiantusset/pedidosapp/PedidosappApplication.java
@@ -1,12 +1,30 @@
 package com.christiantusset.pedidosapp;
 
+import com.christiantusset.pedidosapp.domain.Categoria;
+import com.christiantusset.pedidosapp.repositories.CategoriaDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class PedidosappApplication {
+public class PedidosappApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaDAO repoCategoria;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+
+		repoCategoria.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
